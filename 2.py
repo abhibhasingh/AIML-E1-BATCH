8a35a0933adad30b8307d6e11d1facc1217aa719
abhibhasingh <2024ci_abhibhasingh_a@nie.ac.in>
2026-03-10 15:41:29 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,25 @@
+# Simple Reflex Agent for Vacuum Cleaner World
+
+def reflex_vacuum_agent(location, status):
+    if status == "Dirty":
+        return "Suck"
+    elif location == "A":
+        return "Right"
+    elif location == "B":
+        return "Left"
+    
+location = "A"
+status = "Dirty"
+locations = ["A", "B"]
+
+for _ in range(5):  
+   
+    action = reflex_vacuum_agent(location, status)
+    print(f"Location: {location}, Status: {status}, Action: {action}")
+
+    if action == "Suck":
+        status = "Clean"
+    elif action == "Right":
+        location = "B"
+    elif action == "Left":
+        location = "A"
---
+++ 3.py
@@ -0,0 +1,10 @@
+def move_towards_goal(position, goal):
+    while position < goal:
+        position += 1
+        print(f"Current Position: {position}")
+    print("Reached the goal!")
+
+initial_position = 0
+goal_position = 5
+
+move_towards_goal(initial_position, goal_position)
